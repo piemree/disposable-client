c604d892fc00770a473cd2b141dc3ab19bcc317f
Emre Demir <pi.emree@gmail.com>
2021-08-04 02:06:41 +0300

    Create nav items and drawer

---
+++ components/Navigation.vue
@@ -0,0 +1,103 @@
+<template>
+  <nav>
+    <v-navigation-drawer v-model="drawer" absolute temporary right app>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-row align="center" justify="center" class="flex-nowrap">
+        <v-col cols="6">
+          <v-row align="center" justify="space-between">
+            <div
+              style="width: 100%;height100%"
+              class="d-flex flex justify-space-between align-center"
+            >
+              <v-toolbar-items
+                style="width: 70%; min-width: 200px"
+                class="fill-height d-flex align-center"
+              >
+                <v-text-field
+                  color="#5e5e5e"
+                  small
+                  single-line
+                  v-model="search"
+                  placeholder="Search"
+                  outlined
+                  rounded
+                  dense
+                  prepend-inner-icon="mdi-magnify"
+                  clearable
+                  hide-details
+                ></v-text-field>
+              </v-toolbar-items>
+              <v-btn
+                class="ml-3"
+                width="6rem"
+                elevation="0"
+                rounded
+                color="error"
+              >
+                <v-icon left> mdi-camera </v-icon>
+                <span class="font-weight-bold">Sell</span></v-btn
+              >
+              <v-btn
+                class="ml-3"
+                width="6rem"
+                elevation="0"
+                outlined
+                rounded
+                color="error"
+                ><span class="font-weight-bold">Log In</span></v-btn
+              >
+              <v-btn icon @click.stop="drawer = !drawer">
+                <v-icon>mdi-menu</v-icon>
+              </v-btn>
+            </div>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      search: '',
+      drawer: false,
+      clipped: false,
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+    }
+  },
+}
+</script>
